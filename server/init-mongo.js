1ccf8856caa0db42c1f52fae731062abe7a6f711
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-09-24 11:47:50 +0200

    🌱 Remove unused tokens collection and related index creation from MongoDB initialization

--- server/init-mongo.js
+++ server/init-mongo.js
@@ -5,16 +5,11 @@ db.createUser({
     roles: ["readWrite", "dbAdmin"]
 });
 
-db.createCollection("tokens", {});
-
-const tokensCollection = db.tokens;
-tokensCollection.createIndex({ createdAt: 1 }, { expireAfterSeconds: 600 });
-
 const usersCollection = db.users;
 usersCollection.insertOne({
     email: "admin@admin.com",
     password: "$2a$10$n.TpGvHal2b7gopgn9AErOrMKKTBm.LalREe.SaZzRHMoyGOZYXGG",
     role: "admin",
-    name: "Administrator",
-    id: 0
+    name: "Administrator"
 });
+usersCollection.createIndex({ email: 1 }, { unique: true });
