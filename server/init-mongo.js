6a934cfb3bcf8ad45a44be1b0c2475f74e6142d3
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-09-29 19:02:44 +0200

    🌱 Remove redundant 'name' field from admin user creation in init-mongo.js

--- server/init-mongo.js
+++ server/init-mongo.js
@@ -9,8 +9,7 @@ const usersCollection = db.users;
 usersCollection.insertOne({
     email: "admin@admin.com",
     password: "$2a$10$n.TpGvHal2b7gopgn9AErOrMKKTBm.LalREe.SaZzRHMoyGOZYXGG",
-    role: "admin",
-    name: "Administrator"
+    role: "admin"
 });
 usersCollection.createIndex({ email: 1 }, { unique: true });
 
