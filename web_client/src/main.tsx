0024473be92fdde9c04ba82e464f09d05bcb7b63
KyllianChandelier <kyllian.chandelier@epitech.eu>
2025-10-01 11:10:54 +0200

    ✨ add third party authentication with google

--- web_client/src/main.tsx
+++ web_client/src/main.tsx
@@ -4,13 +4,17 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "./App";
 import Explore from "./pages/Explore";
 import Layout from "./components/Layout";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
+const CLIENT_ID = "909777133411-taulnja2d04q6vd0e2n7105n0erkk4uv.apps.googleusercontent.com";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <GoogleOAuthProvider clientId={CLIENT_ID}>
       <Routes>
         <Route path="/" element={<Layout />}>
           {/* Redirect `/` to `/explore` */}
@@ -23,6 +27,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           <Route path="*" element={<App />} />
         </Route>
       </Routes>
+      </GoogleOAuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
