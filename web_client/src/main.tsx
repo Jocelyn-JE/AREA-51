ae777ec04ada041928af9102085bf6ff6e46dd9d
KyllianChandelier <kyllian.chandelier@epitech.eu>
2025-10-07 15:18:22 +0200

    🧐 change client id for google auth

--- web_client/src/main.tsx
+++ web_client/src/main.tsx
@@ -9,7 +9,7 @@ import "./index.css";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
-const CLIENT_ID = "909777133411-taulnja2d04q6vd0e2n7105n0erkk4uv.apps.googleusercontent.com";
+const CLIENT_ID = "210212748639-u6rbif83ca1uqkijrpc3iak87ajahrpd.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
