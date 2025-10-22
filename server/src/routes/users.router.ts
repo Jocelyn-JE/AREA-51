67ef0a7332e4e28383697a8b2cdbfc9bd32538ec
Azertoxe <tom.feldkamp@epitech.eu>
2025-10-22 16:27:31 +0200

    :bug: fix a bug where the userwas not found cause of a wrong type. (TS shit)

--- server/src/routes/users.router.ts
+++ server/src/routes/users.router.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { ObjectId } from "mongodb";
 import { verifyToken } from "../utils/jwt";
 import { db } from "../mongodb";
 import { User } from "./register.router";
@@ -6,20 +7,20 @@ import { User } from "./register.router";
 const router = express.Router();
 
 // GET /users/info
-router.use("/info", verifyToken, async (req, res) => {
-    if (!req.userId) return res.status(401).json({ error: "Unauthorized" });
+router.get("/info", verifyToken, async (req, res) => {
     try {
-        const userInfo = await db
-            .collection<User>("users")
-            .findOne(
-                { _id: req.userId },
-                { projection: { password: 0, googleId: 0, githubId: 0 } }
-            );
-        if (!userInfo) return res.status(404).json({ error: "User not found" });
-        res.status(200).json(userInfo);
+        const userObjectId = typeof req.userId === "string" 
+            ? new ObjectId(req.userId) 
+            : req.userId;
+
+        const user = await db.collection<User>("users").findOne(
+            { _id: userObjectId },
+            { projection: { password: 0, googleId: 0, githubId: 0 } }
+        );
+        res.send(user);
     } catch (error) {
         console.error("Error fetching user info:", error);
-        res.status(500).json({ error: "Internal server error" });
+        res.status(500).send({ message: "Internal server error" });
     }
 });
 
